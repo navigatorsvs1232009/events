e9a246badde3d84f6599ca84bccfb0861165f779
Konstantin Savin <ks5470219@gmail.com>
2024-01-18 14:08:25 +0700

    Update CrmDeal.php

--- app/EventHandlers/CRM/CrmDeal.php
+++ app/EventHandlers/CRM/CrmDeal.php
@@ -2,20 +2,11 @@
 
 namespace EventHandlers\CRM;
 use \Bitrix\Crm\DealTable;
-
-function writeToLog($data, $title = '') {
-    $log = "\n------------------------\n";
-    $log .= date("Y.m.d G:i:s") . "\n";
-    $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
-    $log .= print_r($data, 1);
-    $log .= "\n------------------------\n";
-    file_put_contents(getcwd() . '/hook.log', $log, FILE_APPEND);
-    return true;}
-
+use Bitrix\Main\Diag\Debug;
 abstract class CrmDeal {
     public static function onAfterUpdate(array $deal)
     {
         DealTable::update($deal['ID'], ['TITLE' => 'Обновлено']);
-        writeToLog($deal, 'incoming');
+        Debug::writeToFile($deal);
     }
 }
